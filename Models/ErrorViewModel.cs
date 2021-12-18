72422ad0db5dd52a1d021c113ddf178238a8c423
ilyaik <ilya-kyp@yandex.ru>
2021-12-18 19:47:21 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kupri4.ShopCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
