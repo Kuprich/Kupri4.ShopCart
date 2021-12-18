0d453cd27fdef0a9b4186bddccb7fef3be9c5902
ilyaik <ilya-kyp@yandex.ru>
2021-12-18 19:51:01 +0300

    Добавлен контекст базы данных

---
+++ Infrastructure/ShopCartDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kupri4.ShopCart.Infrastructure
+{
+    public class ShopCartDbContext : DbContext
+    {
+        public ShopCartDbContext(DbContextOptions<ShopCartDbContext> options)
+            : base(options)
+        { }
+    }
+}
