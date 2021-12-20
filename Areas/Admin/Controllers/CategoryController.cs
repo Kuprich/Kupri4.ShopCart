f5e0ccb0bc8ef7b09347059289052334e56e4791
ilyaik <ilya-kyp@yandex.ru>
2021-12-20 19:54:02 +0300

    Создана модель Category и пустой контроллер

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Kupri4.ShopCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kupri4.ShopCart.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ShopCartDbContext _dbContext;
+
+        public CategoryController(ShopCartDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kupri4.ShopCart.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(2, ErrorMessage = "Minimum lenght is 2")]
+        [RegularExpression(@"^[a-zA-Zа-яА-я]+$", ErrorMessage = "Only letters are allowed")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Slug { get; set; }
+
+        public int Sorting { get; set; }
+    }
+}
