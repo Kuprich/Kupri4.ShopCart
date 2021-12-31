33d9b8aed331150a754f1d1c6cc66df1c0d9f815
ilyaik <ilya-kyp@yandex.ru>
2021-12-31 14:46:19 +0300

    Создан ViewComponent "MainMenu" для вывода списка страниц

---
+++ Infrastructure/Components/MainMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Kupri4.ShopCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kupri4.ShopCart.Infrastructure.Components
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+
+        private readonly ShopCartDbContext _dbContext;
+
+        public MainMenuViewComponent(ShopCartDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => 
+            View(await _dbContext.Pages.OrderBy(x => x.Sorting).ToListAsync());
+
+    }
+}
