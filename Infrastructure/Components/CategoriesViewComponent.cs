0831ce357c90a6555fd41cb4838e1be15083e982
ilyaik <ilya-kyp@yandex.ru>
2022-01-02 20:11:26 +0300

    Создан ViewComponent Categories
    использован для навигации по категориям

---
+++ Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kupri4.ShopCart.Infrastructure.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly ShopCartDbContext _dbContext;
+
+        public CategoriesViewComponent(ShopCartDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() =>
+           View(await _dbContext.Categories.OrderBy(x => x.Sorting).ToListAsync());
+    }
+}
