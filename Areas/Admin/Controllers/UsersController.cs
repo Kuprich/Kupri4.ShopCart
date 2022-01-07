b879314c2d3bf2174d128fa028dace1dde1f2a3e
ilyaik <ilya-kyp@yandex.ru>
2022-01-07 20:19:56 +0300

    Создан контроллер Users и представление, отображающее всех пользователей

---
+++ Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Kupri4.ShopCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kupri4.ShopCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UsersController(UserManager<AppUser> userManager) => 
+            _userManager = userManager;
+
+        public ViewResult Index() =>
+            View(_userManager.Users);
+    }
+}
